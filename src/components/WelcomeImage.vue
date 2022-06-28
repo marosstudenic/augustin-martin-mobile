a1b300c92233b2c93ecb8ba8b7a87f803ab804a6
maros Studenic <studenic.maros@gmail.com>
2022-06-28 02:17:19 +0200

    add images, and resize them

--- src/components/WelcomeImage.vue
+++ src/components/WelcomeImage.vue
@@ -2,7 +2,7 @@
   <section id="welcome-image">
     <img
       src="../assets/images/welcome-images/cervene_zahrady.jpg"
-      alt="TODO"
+      alt="obraz cervene_zahrady"
       class="w-screeen h-[calc(100vh-4rem)] object-cover"
     />
   </section>
